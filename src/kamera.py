c45917851ea0240eaed509ae4d473a2382086e56
Pecneb <ecneb2000@gmail.com>
2021-09-17 21:47:40 +0200

    Kamera megjelenites

---
+++ src/kamera.py
@@ -0,0 +1,28 @@
+import matplotlib.pyplot as plt
+import numpy
+import cv2
+
+def main():
+    cap = cv2.VideoCapture(0)
+    if not cap.isOpened():
+        print("Cannot open camera")
+        exit()
+    while True:
+        ret, frame = cap.read()
+        
+        if not ret:
+            print("Cant recieve frame (stream end?). Exiting ...")
+            break
+        
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        cv2.imshow('frame', gray)
+        if cv2.waitKey(1) == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+if __name__=="__main__":
+    main()
+    
