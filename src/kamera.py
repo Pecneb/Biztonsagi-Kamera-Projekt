b468fc1bb06e350d6a23e9d850ebda1b9b0f5160
Pecneb <57943524+Pecneb@users.noreply.github.com>
2021-09-17 22:34:18 +0200

    adding comments to kamera.py

--- src/kamera.py
+++ src/kamera.py
@@ -3,19 +3,28 @@ import numpy
 import cv2
 
 def kamera(vsrc):
+    # getting video source
     cap = cv2.VideoCapture(vsrc)
+    
+    # generating warning message if couldnt open video
     if not cap.isOpened():
         print("Cannot open camera")
         exit()
+        
+    # showing video, frame by frame
     while True:
         ret, frame = cap.read()
         
+        # checking if theres any frames recieved
         if not ret:
             print("Cant recieve frame (stream end?). Exiting ...")
             break
         
         # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        # showing frame
         cv2.imshow('frame', frame)
+        
+        # waiting for exit key, which in this case is 'Q'
         if cv2.waitKey(1) == ord('q'):
             break
 
