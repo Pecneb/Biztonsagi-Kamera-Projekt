a9534754d27b9d042eeb3f9f53fe6582fea0b89d
Pecneb <ecneb2000@gmail.com>
2021-09-17 22:06:12 +0200

    command line arg vidoe src or kamera src

--- src/main.py
+++ src/main.py
@@ -4,13 +4,17 @@ import argparse
 import cv2 as cv
 import numpy
 import matplotlib.pyplot as plt
+from kamera import kamera
 
 def main():
     parser = argparse.ArgumentParser(description='Security Camera with motion detection:')
     parser.add_argument('vsrc', metavar='[video source path]', help='path to the video source')
     args = parser.parse_args()
-    print(args.vsrc)
     
+    if args.vsrc in '0123456789':
+        kamera(int(args.vsrc))
+
+    kamera(args.vsrc)
 
 if __name__=="__main__":
     main()
