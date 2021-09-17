b3343a16ba15931f92fb9b6baa1a020da3486fc6
Pecneb <57943524+Pecneb@users.noreply.github.com>
2021-09-17 22:30:43 +0200

    adding comments to main.py

--- src/main.py
+++ src/main.py
@@ -7,13 +7,17 @@ import matplotlib.pyplot as plt
 from kamera import kamera
 
 def main():
+    # Using argparser to get command line arguments
     parser = argparse.ArgumentParser(description='Security Camera with motion detection:')
+    # vsrc is an argument, the user should give in the command line, no matter what!
     parser.add_argument('vsrc', metavar='[video source path]', help='path to the video source')
+    # parse args
     args = parser.parse_args()
     
+    # if user gives video src 0-9, then its propably a kamera
     if args.vsrc in '0123456789':
         kamera(int(args.vsrc))
-
+    # if not, then its a video
     kamera(args.vsrc)
 
 if __name__=="__main__":
