f13595a8473b2cb0645879d6bb9f5b278e3e01b3
Pecneb <ecneb2000@gmail.com>
2021-09-23 23:12:47 +0200

    main module update

--- src/main.py
+++ src/main.py
@@ -5,20 +5,30 @@ import cv2 as cv
 import numpy
 import matplotlib.pyplot as plt
 from kamera import kamera
+from bgsub import bgsub
 
 def main():
     # Using argparser to get command line arguments
-    parser = argparse.ArgumentParser(description='Security Camera with motion detection:')
+    parser = argparse.ArgumentParser(description="Security Camera with motion detection.(quit with 'q')")
     # vsrc is an argument, the user should give in the command line, no matter what!
-    parser.add_argument('vsrc', metavar='[video source path]', help='path to the video source')
+    parser.add_argument('vsrc', metavar='[video source path]', help='path to the video source (0 to use webcam).')
+    # mode: kamera or background subtraction
+    parser.add_argument('--mode', help='Choose: show camera/video (play) or background subtraction (md) mode.', default='cam')
+    # choose algo for background subtraction
+    parser.add_argument('--algo', help='Choose what background subtractor algo to use: MOG2 or KNN', default='MOG2')
     # parse args
     args = parser.parse_args()
     
-    # if user gives video src 0-9, then its propably a kamera
-    if args.vsrc in '0123456789':
-        kamera(int(args.vsrc))
-    # if not, then its a video
-    kamera(args.vsrc)
+    if args.mode == 'play':
+        if args.vsrc == '0':
+            kamera((int)(args.vsrc))
+        else:
+            kamera(args.vsrc)
+    elif args.mode == 'md':
+        if args.vsrc == '0':
+            bgsub((int)(args.vsrc), args.algo)
+        else:
+            bgsub(args.vsrc, args.algo)
 
 if __name__=="__main__":
     main()
