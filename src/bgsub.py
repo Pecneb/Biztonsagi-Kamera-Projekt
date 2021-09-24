eedcc96833910ae9c4d59289e6ad4e85390a3090
Pecneb <ecneb2000@gmail.com>
2021-09-24 18:02:01 +0200

    motion sensing logic inserted to bgsub

--- src/bgsub.py
+++ src/bgsub.py
@@ -1,5 +1,15 @@
 import cv2 as cv
+import numpy as np
+from matplotlib import pyplot as plt
+from oqqupation import is_oqqupied
 
+GREEN = [0,255,0]
+RED = [0,0,255]
+
+
+'''
+Background subtraction module
+'''
 def bgsub(vsrc, algo):
     
     if algo == 'MOG2':
@@ -8,22 +18,28 @@ def bgsub(vsrc, algo):
         backSub = cv.createBackgroundSubtractorKNN()
     capture = cv.VideoCapture(vsrc)
     if not capture.isOpened():
-        print('Unable to open: ' + args.input)
+        print('Unable to open: ' + vsrc)
         exit(0)
     while True:
         ret, frame = capture.read()
         if frame is None:
             break
         
-        fgMask = backSub.apply(frame)
+        fgMask = backSub.apply(frame, learningRate=-1)
+        bgIm = backSub.getBackgroundImage()
         
-        
+        if is_oqqupied(fgMask, 1):
+            border = cv.copyMakeBorder(frame, 10,10,10,10,cv.BORDER_CONSTANT, value=GREEN)
+        else:
+            border = cv.copyMakeBorder(frame, 10,10,10,10,cv.BORDER_CONSTANT, value=RED)
+
+    
         cv.rectangle(frame, (10, 2), (100,20), (255,255,255), -1)
         cv.putText(frame, str(capture.get(cv.CAP_PROP_POS_FRAMES)), (15, 15),
-                cv.FONT_HERSHEY_SIMPLEX, 0.5 , (0,0,0))
+                    cv.FONT_HERSHEY_SIMPLEX, 0.5 , (0,0,0))
         
         
-        cv.imshow('Frame', frame)
+        cv.imshow('Frame', border)
         cv.imshow('FG Mask', fgMask)
         
         # waiting for exit key, which in this case is 'Q'
