6b2b30f61458ae727dd5c904b15779d276162278
Pecneb <ecneb2000@gmail.com>
2021-10-05 19:30:03 +0200

    Early implementation of motion tracking.

---
+++ src/track_motion.py
@@ -0,0 +1,32 @@
+import cv2 as cv
+
+def track_motion(frame, mask):
+    '''
+    This module takes frame and mask arguments.
+    Frame is a frame from a video src.
+    The mask is the area of a motion on the video frame.
+    From these information, the module will track the object on the screen.
+    '''
+    
+    # initialize location of tracking window
+    x, y, w, h = 300, 200, 200, 120
+    track_window = (x, y, w, h)
+
+    # calc hist 
+    mhist = cv.calcHist([mask], [0], None, [2], [0,256])
+    
+    # range of interest
+    roi = mask[y:y+h, x:x+w]
+    term_crit =( cv.TERM_CRITERIA_EPS | cv.TermCriteria_COUNT, 10, 1)
+
+    # calc backproject
+    dst = cv.calcBackProject([mask], [0], mhist, [10,255], 1)
+    
+    # apply meanshift for object tracking
+    # it gives back the tracking window parameters
+    ret, track_window = cv.CamShift(dst, track_window, term_crit)
+    x, y, w, h = track_window
+    return x,y,w,h
+
+
+
