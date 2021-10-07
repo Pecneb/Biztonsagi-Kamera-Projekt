b7f36dc7d20c25e76fef84bb61e1a3782f3da1ba
Pecneb <ecneb2000@gmail.com>
2021-10-07 17:39:20 +0200

    Fine tuning sensitivity

--- src/oqqupation.py
+++ src/oqqupation.py
@@ -7,7 +7,13 @@ import os
 Tells if there motion in the frame or not.
 '''
 def is_oqqupied(frame, threshold):
+    '''
+    Frame: the input image, this should be a foreground mask
+    Threshold: with this argument, we can adjust the sensitivity of the detection in range 0 - 10000.
+        the smaller the number, the smaller motion can be detected. 
+    '''
     # img = cv.imread('./Back_Projection_Theory0.jpg')
+    #calculate histogram of the image
     hist = cv.calcHist([frame], [0], None, [2], [0,256])
     # plt.figure()
     # plt.title("Hist")
@@ -17,9 +23,9 @@ def is_oqqupied(frame, threshold):
     # plt.xlim([0,256])
     # plt.show()
     summa = sum(hist[:,0])
-    print(hist[1])
+    # print(hist[1])
     # print(f"Over 50: {oth0} \t Under 50: {uth1} And {summa}")
-    if (hist[1]/summa)*100 > threshold:
+    if (hist[1]/summa)*10000 > threshold:
         return True
     return False
 
