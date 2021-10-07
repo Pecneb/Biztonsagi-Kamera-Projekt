5cc7c641aad4ef4db9c007cb9f2afe3de0409588
Pecneb <ecneb2000@gmail.com>
2021-10-07 17:38:47 +0200

    Updated args, and fine tuning

--- src/bgsub.py
+++ src/bgsub.py
@@ -3,6 +3,8 @@ import numpy as np
 from matplotlib import pyplot as plt
 from oqqupation import is_oqqupied
 from track_motion import track_motion
+from find_anomaly import find_anomaly
+from track_motion2 import track_motion2
 
 GREEN = [0,255,0]
 RED = [0,0,255]
@@ -11,41 +13,59 @@ RED = [0,0,255]
 '''
 Background subtraction module
 '''
-def bgsub(vsrc, algo):
-    
-    if algo == 'MOG2':
+def bgsub(vsrc, bgAlgo, sensAlgo):
+    # choose what algorythm to use: MOG2 or KKN
+    if bgAlgo == 'MOG2':
         backSub = cv.createBackgroundSubtractorMOG2()
     else:
         backSub = cv.createBackgroundSubtractorKNN()
+    
+    # get video from vsrc
     capture = cv.VideoCapture(vsrc)
+    # check if video can be opened
     if not capture.isOpened():
         print('Unable to open: ' + vsrc)
         exit(0)
+    # play video by frame by frame
     while True:
+        # read frame from capture obj
         ret, frame = capture.read()
         if frame is None:
             break
         
+        # apply background subtrcation algorythm on frame
         fgMask = backSub.apply(frame, learningRate=-1)
+        # try to remove shadows with threshholding
+        ret, fgMask = cv.threshold(fgMask, 128, 255, cv.THRESH_BINARY)
+        # this is the background image, we dont really use it, but if we need it, its there
         bgIm = backSub.getBackgroundImage()
-        # blurring mask image, to decrease noice
-        fgMask = cv.GaussianBlur(fgMask,(21,21), 0)
-
+        # blurring mask image, to decrease noice, with Gaussian Blur
+        fgMask = cv.GaussianBlur(fgMask,(9,9), 0)
+        # fgMask = cv.dilate(fgMask, None,  iterations=3)
+        # check if there is any motion in the frame
         if is_oqqupied(fgMask, 1):
+            # if theres any motion, draw green border around the frame
             border = cv.copyMakeBorder(frame, 10,10,10,10,cv.BORDER_CONSTANT, value=GREEN)
-            # applying the motion tracker module
-            x,y,w,h = track_motion(frame, fgMask)
-            # drawing border around detected motion
-            border = cv.rectangle(border, (x, y), (x+w, y+h), 255, 2)
+            if sensAlgo == 'tm':
+                # applying the motion tracker module
+                x,y,w,h = track_motion(frame, fgMask)
+                # drawing border around detected motion
+                border = cv.rectangle(border, (x, y), (x+w, y+h), 255, 2)
+            if sensAlgo == 'tm2':
+                # apply canny algorythm
+                track_motion2(border, fgMask)
         else:
+            # if theres no motion, draw red border around the frame
             border = cv.copyMakeBorder(frame, 10,10,10,10,cv.BORDER_CONSTANT, value=RED)
 
-    
-        cv.rectangle(frame, (10, 2), (100,20), (255,255,255), -1)
-        cv.putText(frame, str(capture.get(cv.CAP_PROP_POS_FRAMES)), (15, 15),
-                    cv.FONT_HERSHEY_SIMPLEX, 0.5 , (0,0,0))
+
+        # cv.rectangle(frame, (10, 2), (100,20), (255,255,255), -1)
+        # cv.putText(frame, str(capture.get(cv.CAP_PROP_POS_FRAMES)), (15, 15),
+        #             cv.FONT_HERSHEY_SIMPLEX, 0.5 , (0,0,0))
         
-        
+        # edges = cv.Canny(fgMask, 127, 255)
+        # show frames with imshow
+        # cv.imshow('Canny', edges)
         cv.imshow('Frame', border)
         cv.imshow('FG Mask', fgMask)
         
