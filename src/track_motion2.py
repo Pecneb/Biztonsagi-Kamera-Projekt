79bb9882b7175f49a2f935e8cb2b6d6e9a6c243a
Pecneb <ecneb2000@gmail.com>
2021-10-11 17:32:45 +0200

    Document logic through commenting lines

--- src/track_motion2.py
+++ src/track_motion2.py
@@ -9,19 +9,26 @@ def track_motion2(frame, mask):
     Mask is the mask from the backgoundsubtraction. 
     '''
     
-    # oframe = cv.cvtColor(mask, cv.COLOR_BGR2GRAY)
-    blur = cv.GaussianBlur(mask, (7,7), 0)
-    _, thresh = cv.threshold(blur, 150, 255, cv.THRESH_BINARY)
-    dilate = cv.dilate(thresh, None, iterations=2)
-    contours, hierarchy = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+    # Remove noice
+    blur = cv.GaussianBlur(mask, (9,9), 0)
+    
+    # Remove false positives, with threshold
+    _, thresh = cv.threshold(blur, 200, 255, cv.THRESH_BINARY)
+    
+    # Enhance moving object
+    dilate = cv.dilate(thresh, None, iterations=3)
+    
+    # Find objects with contour detection
+    contours, hierarchy = cv.findContours(dilate, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
     # ret_img = cv.drawContours(frame, ret_img, -1, (0,0,255), 1)
 
+    # Iterate through objects, and draw rectangle around them
     for contour in contours:
         # get rectangle position
         (x,y,w,h) = cv.boundingRect(contour)
     
         # reduce motion noice
-        if cv.contourArea(contour) > 200:
+        if cv.contourArea(contour) > 300:
             # print contour area sizes for debug purposes
             print(cv.contourArea(contour))
 
