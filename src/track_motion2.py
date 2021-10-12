6d90227ba3433296b6196094d636e54f48d16969
Pecneb <ecneb2000@gmail.com>
2021-10-12 13:52:10 +0200

    Still fine tuning, cant find the sweet spot in noice reduction

--- src/track_motion2.py
+++ src/track_motion2.py
@@ -16,13 +16,13 @@ def track_motion2(frame, mask):
     _, thresh = cv.threshold(blur, 160, 255, cv.THRESH_BINARY)
     
     # Enhance moving object
-    dilate = cv.dilate(thresh, None, iterations=2)
-    morph = cv.morphologyEx(thresh, cv.MORPH_OPEN, None, iterations=2)
-    morph = cv.morphologyEx(morph, cv.MORPH_CLOSE, None, iterations=2)
-    
+    morph = cv.morphologyEx(thresh, cv.MORPH_OPEN, None, iterations=3)
+    # morph = cv.morphologyEx(morph, cv.MORPH_CLOSE, None, iterations=3)
+    morph = cv.dilate(thresh, None, iterations=2)
+
     # Find objects with contour detection
     contours, hierarchy = cv.findContours(morph, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
-    # cv.drawContours(frame, contours, -1, 255, 2)
+    cv.drawContours(mask, contours, -1, (0,255,0), 50, hierarchy=hierarchy, maxLevel=1)
 
     # Iterate through objects, and draw rectangle around them
     for contour in contours:
@@ -32,7 +32,7 @@ def track_motion2(frame, mask):
         # reduce motion noice
         if cv.contourArea(contour) > 350:
             # print contour area sizes for debug purposes
-            print(cv.contourArea(contour))
+            # print(cv.contourArea(contour))
 
             # draw rectangle on original frame
             cv.rectangle(frame, (x,y), (x+w, y+h), (0,0,255), 2)
