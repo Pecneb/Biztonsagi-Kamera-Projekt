8af524254e56a0e488bc53eee000de5189293ced
Pecneb <ecneb2000@gmail.com>
2021-11-09 22:20:47 +0100

    Preparing to merge to master

--- src/camshift.py
+++ src/camshift.py
@@ -1,7 +1,7 @@
 from typing import Annotated
 import cv2 as cv
 
-def track_motion(frame, mask):
+def track_motion(frame, mask, initial_track_window = ()):
     '''
     This module takes frame and mask arguments.
     Frame is a frame from a video src.
@@ -11,23 +11,27 @@ def track_motion(frame, mask):
     x,y coordinates with w=width, h=height
     '''
     # initialize location of tracking window
-    x, y, w, h = 300, 200, 200, 120
+    x, y, w, h = initial_track_window
     track_window = (x, y, w, h)
 
+    # prepare frame
+    img = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
+
+
     # calc hist 
-    mhist = cv.calcHist([mask], [0], None, [2], [0,256])
+    mhist = cv.calcHist([img], [0], None, [2], [0,256])
     
     # range of interest
-    roi = mask[y:y+h, x:x+w]
+    roi = img[y:y+h, x:x+w]
     term_crit =( cv.TERM_CRITERIA_EPS | cv.TermCriteria_COUNT, 10, 1)
 
     # calc backproject
-    dst = cv.calcBackProject([mask], [0], mhist, [10,255], 1)
+    dst = cv.calcBackProject([img], [0], mhist, [10,255], 1)
     
     # apply meanshift for object tracking
     # it gives back the tracking window parameters
-    ret, track_window = cv.CamShift(dst, track_window, term_crit)
-    x, y, w, h = track_window
+    ret, new_position_of_object = cv.CamShift(dst, track_window, term_crit)
+    x, y, w, h = new_position_of_object
     
     return x,y,w,h
 
